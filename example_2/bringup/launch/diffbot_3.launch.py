5254f65360f23a7fc5faa5c9392bd3a957723afb
Juan Ramirez Jardua <lch_jramirez@hotmail.com>
2025-06-01 19:20:47 -0400

    small changes

--- example_2/bringup/launch/diffbot_3.launch.py
+++ example_2/bringup/launch/diffbot_3.launch.py
@@ -125,7 +125,10 @@ def generate_launch_description():
         output="both",
         parameters=[robot_description],
         remappings=[
-            ("/diff_drive_controller/cmd_vel_unstamped", "/cmd_vel"),
+           # ("/diff_drive_controller/cmd_vel_unstamped", "/cmd_vel"),
+            # ("/diffbot_base_controller/cmd_vel_unstamped", "/cmd_vel"),
+            # ("/diffbot_base_controller/odom","/odom"),
+            
         ],
     )
     rviz_node = Node(
